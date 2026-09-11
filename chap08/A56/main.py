be49f0915c8afd547c4354771ba9ae91a57bd8d9
tomo <o_tomo_1990@yahoo.co.jp>
2026-09-11 20:17:47 +0900

    refactor A56

--- chap08/A56/main.py
+++ chap08/A56/main.py
@@ -1,44 +1,34 @@
+class Hash:
+    def __init__(self, S: str) -> None:
+        self.mod = 2147483647
+        self.s = S
+        self.t = [ord(s) for s in self.s]
+        self.b = [1] * len(S)
+        for i in range(1, len(S)):
+            self.b[i] = 100 * self.b[i - 1] % self.mod
+        self.h = [0] * len(S)
+        self.h[0] = self.t[0]
+        for i in range(1, len(S)):
+            self.h[i] = (100 * self.h[i - 1] + self.t[i]) % self.mod
+
+    def hash(self, l: int, r: int):
+        if l == 0:
+            return self.h[r]
+        value = self.h[r] - self.h[l - 1] * self.b[r - l + 1]
+        return value % self.mod
+
+
 N, Q = map(int, input().split())
 S = input()
-mod = 2147483647
-# print(ord("a"))
-T = [ord(s) - ord("a") + 1 for s in S]
-# print(T)
-Power100 = [1] * N
-for i in range(1, N):
-    Power100[i] = 100 * Power100[i - 1] % mod
 
-# print(Power100)
-
-H: list[int] = [0] * N
-for i in range(N):
-    if i == 0:
-        H[i] = T[i]
-        continue
-    H[i] = (100 * H[i - 1] + T[i]) % mod
-
-# print(H[-1])
-# print(H)
-
-
-def hash(l: int, r: int):
-    if l == 0:
-        return H[r]
-    value = H[r] - (H[l - 1] * Power100[r - l + 1] % mod)
-    if value < 0:
-        value += mod
-    return value
-
-
+hash = Hash(S)
 for _ in range(Q):
     a, b, c, d = map(int, input().split())
     a -= 1
     b -= 1
     c -= 1
     d -= 1
-    # print(hash(a, b))
-    # print(hash(c, d))
-    if hash(a, b) == hash(c, d):
+    if hash.hash(a, b) == hash.hash(c, d):
         print("Yes")
     else:
         print("No")
