45e2b01b71d9bb1d74e50ebce2b976d49b4267cd
tomo <o_tomo_1990@yahoo.co.jp>
2026-09-11 20:49:13 +0900

    refactor A56, B56

--- chap08/A56/main.py
+++ chap08/A56/main.py
@@ -1,15 +1,14 @@
 class Hash:
     def __init__(self, S: str) -> None:
         self.mod = 2147483647
-        self.s = S
-        self.t = [ord(s) for s in self.s]
+        t = [ord(s) for s in S]
         self.b = [1] * len(S)
         for i in range(1, len(S)):
             self.b[i] = 100 * self.b[i - 1] % self.mod
         self.h = [0] * len(S)
-        self.h[0] = self.t[0]
+        self.h[0] = t[0]
         for i in range(1, len(S)):
-            self.h[i] = (100 * self.h[i - 1] + self.t[i]) % self.mod
+            self.h[i] = (100 * self.h[i - 1] + t[i]) % self.mod
 
     def hash(self, l: int, r: int):
         if l == 0:
